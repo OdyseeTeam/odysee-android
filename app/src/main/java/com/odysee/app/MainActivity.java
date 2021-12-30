5873e69e6b911191bf8ebd0fb390ec1a009ff330
Javi Rueda <foss@franjaru.com>
2021-12-30 22:29:32 +0100

    Fix navigation item selected listener deprecation warning (#70)

--- app/src/main/java/com/odysee/app/MainActivity.java
+++ app/src/main/java/com/odysee/app/MainActivity.java
@@ -81,6 +81,7 @@ import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
 import com.google.android.material.bottomnavigation.BottomNavigationView;
 import com.google.android.material.button.MaterialButton;
+import com.google.android.material.navigation.NavigationBarView;
 import com.google.android.material.snackbar.Snackbar;
 import com.google.firebase.messaging.FirebaseMessaging;
 
@@ -592,7 +593,7 @@ public class MainActivity extends AppCompatActivity implements SharedPreferences
         fragmentManager.addOnBackStackChangedListener(backStackChangedListener);
 
         BottomNavigationView bottomNavigation = findViewById(R.id.bottom_navigation);
-        bottomNavigation.setOnNavigationItemSelectedListener(new BottomNavigationView.OnNavigationItemSelectedListener() {
+        bottomNavigation.setOnItemSelectedListener(new NavigationBarView.OnItemSelectedListener() {
             @Override
             public boolean onNavigationItemSelected(@NonNull MenuItem item) {
                 Fragment selectedFragment;
