cb2fc657ca376c4afd8274183a17fb05ee4ea776
Javi Rueda <foss@franjaru.com>
2022-03-22 12:00:11 +0100

    Replace WalletBalanceTask with a callable (#147)

--- app/src/main/java/com/odysee/app/adapter/CommentAction.java
+++ app/src/main/java/com/odysee/app/adapter/CommentAction.java
@@ -5,7 +5,6 @@ import android.accounts.AccountManager;
 import android.app.Activity;
 import android.content.Context;
 import android.content.DialogInterface;
-import android.os.AsyncTask;
 import android.view.WindowManager;
 import android.view.inputmethod.InputMethodManager;
 import android.widget.EditText;
