94731fd30e16d619e4f5df42e08d1948992737fc
kt programs <ktprograms@gmail.com>
2022-04-07 15:30:59 +0800

    Add precheck and error handling
    Logic mostly copied verbatim from the iOS code.

--- app/src/main/java/com/odysee/app/ui/golive/GoLiveFragment.java
+++ app/src/main/java/com/odysee/app/ui/golive/GoLiveFragment.java
@@ -9,13 +9,16 @@ import android.content.pm.PackageManager;
 import android.hardware.camera2.CameraCharacteristics;
 import android.os.AsyncTask;
 import android.os.Bundle;
-import android.util.Log;
+import android.os.Handler;
+import android.os.Looper;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.AdapterView;
 import android.widget.ImageButton;
 import android.widget.ImageView;
 import android.widget.Spinner;
+import android.widget.TextView;
 
 import androidx.activity.result.contract.ActivityResultContracts;
 import androidx.annotation.NonNull;
@@ -58,6 +61,7 @@ import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
+import java.util.Optional;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 
@@ -66,14 +70,19 @@ public class GoLiveFragment extends BaseFragment implements
         StoragePermissionListener,
         FilePickerListener {
     private final String RTMP_URL = "rtmp://stream.odysee.com/live";
+    private final double MIN_STREAM_STAKE = 50;
 
+    private ImageView imagePrecheckSpaceman;
+    private TextView textPrecheckStatus;
     private Spinner selectChannelSpinner;
+    private TextView textChannelError;
     private TextInputEditText inputTitle;
-    private View publishLivestreamProgress;
+    private View progress;
     private ImageView imageThumbnail;
     private View thumbnailUploadProgress;
     private MaterialButton buttonToggleStreaming;
 
+    private View livestreamPrecheckView;
     private View livestreamOptionsView;
     private View livestreamControlsView;
 
@@ -84,9 +93,11 @@ public class GoLiveFragment extends BaseFragment implements
     private Camera2Source cameraSource;
 
     private boolean launchPickerPending;
-    private boolean uploading;
+    private boolean uploadingThumbnail;
     private String uploadedThumbnailUrl;
     private String lastSelectedThumbnailFile;
+    private boolean isStreaming = false;
+    private boolean startingStream = false;
 
     @Override
     public void onCreate(@Nullable Bundle savedInstanceState) {
@@ -108,15 +119,31 @@ public class GoLiveFragment extends BaseFragment implements
         connection.addEventListener(Event.RTMP_STATUS, new IEventListener() {
             @Override
             public void handleEvent(@NonNull Event event) {
+                startingStream = false;
                 Object data = event.getData();
                 if (data instanceof Map) {
                     @SuppressWarnings("unchecked")
                     HashMap<String, Object> dataMap = (HashMap<String, Object>) data;
                     String code = dataMap.get("code").toString();
                     if (code.equals(RtmpConnection.Code.CONNECT_SUCCESS.getRawValue())) {
+                        isStreaming = true;
                         stream.publish(streamKey, RtmpStream.HowToPublish.LIVE);
+                    } else if (code.equals(RtmpConnection.Code.CONNECT_FAILED.getRawValue())) {
+                        isStreaming = false;
+                        showError(getString(R.string.stream_connect_failed));
+                    } else if (code.equals(RtmpConnection.Code.CONNECT_CLOSED.getRawValue())) {
+                        // attempt connection retry?
+                        isStreaming = false;
                     }
                 }
+            }
+        });
+        connection.addEventListener(Event.IO_ERROR, new IEventListener() {
+            @Override
+            public void handleEvent(@NonNull Event event) {
+                // simply attempt to reconnect
+                showError(getString(R.string.stream_connect_failed_retrying));
+                connection.connect(RTMP_URL);
             }
         });
     }
@@ -126,12 +153,16 @@ public class GoLiveFragment extends BaseFragment implements
                              Bundle savedInstanceState) {
         View root = inflater.inflate(R.layout.fragment_go_live, container, false);
 
+        imagePrecheckSpaceman = root.findViewById(R.id.livestream_precheck_spaceman_image);
+        textPrecheckStatus = root.findViewById(R.id.livestream_precheck_status_text);
         selectChannelSpinner = root.findViewById(R.id.livestream_options_select_channel_spinner);
+        textChannelError = root.findViewById(R.id.livestream_options_channel_error_text);
         inputTitle = root.findViewById(R.id.livestream_options_title_input);
-        publishLivestreamProgress = root.findViewById(R.id.publish_livestream_progress);
+        progress = root.findViewById(R.id.livestream_progress);
         imageThumbnail = root.findViewById(R.id.livestream_options_thumbnail_preview);
         thumbnailUploadProgress = root.findViewById(R.id.livestream_options_thumbnail_upload_progress);
 
+        livestreamPrecheckView = root.findViewById(R.id.livestream_precheck);
         livestreamOptionsView = root.findViewById(R.id.livestream_options);
         livestreamControlsView = root.findViewById(R.id.livestream_controls);
 
@@ -141,6 +172,19 @@ public class GoLiveFragment extends BaseFragment implements
         MaterialButton buttonContinue = root.findViewById(R.id.livestream_options_continue_button);
         buttonToggleStreaming = root.findViewById(R.id.livestream_controls_toggle_streaming_button);
         ImageButton buttonSwitchCamera = root.findViewById(R.id.livestream_controls_switch_camera_button);
+
+        selectChannelSpinner.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+                selectedChannel = (Claim) selectChannelSpinner.getSelectedItem();
+                checkCanStreamOnChannel(selectedChannel);
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> parent) {
+
+            }
+        });
 
         imageThumbnail.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -152,20 +196,40 @@ public class GoLiveFragment extends BaseFragment implements
         buttonContinue.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
+                if (uploadingThumbnail) {
+                    showError(getString(R.string.publish_thumbnail_in_progress));
+                    return;
+                }
+                if (!checkCanStreamOnChannel(selectedChannel)) {
+                    showError(getString(R.string.need_valid_channel));
+                    return;
+                }
+                if (Helper.isNullOrEmpty(Helper.getValue(inputTitle.getText()))) {
+                    showError(getString(R.string.specify_title));
+                    return;
+                }
+
+                progress.setVisibility(View.VISIBLE);
                 livestreamOptionsView.setVisibility(View.GONE);
-                publishLivestreamClaim();
                 livestreamControlsView.setVisibility(View.VISIBLE);
+                publishLivestreamClaim();
             }
         });
 
         buttonToggleStreaming.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                if (buttonToggleStreaming.getText() == getString(R.string.start_streaming)) {
+                if (startingStream) {
+                    return;
+                }
+                if (!isStreaming) {
+                    startingStream = true;
                     connection.connect(RTMP_URL);
                     buttonToggleStreaming.setText(R.string.stop_streaming);
                 } else {
                     connection.close();
+                    isStreaming = false;
+                    startingStream = false;
                     buttonToggleStreaming.setText(R.string.start_streaming);
                 }
             }
@@ -270,7 +334,7 @@ public class GoLiveFragment extends BaseFragment implements
     }
 
     private void uploadThumbnail(String thumbnailPath) {
-        if (uploading) {
+        if (uploadingThumbnail) {
             View view = getView();
             if (view != null) {
                 Snackbar.make(view, R.string.wait_for_upload, Snackbar.LENGTH_LONG).show();
@@ -283,14 +347,14 @@ public class GoLiveFragment extends BaseFragment implements
             Glide.with(context.getApplicationContext()).load(thumbnailPath).centerCrop().into(imageThumbnail);
         }
 
-        uploading = true;
+        uploadingThumbnail = true;
         uploadedThumbnailUrl = null;
         UploadImageTask task = new UploadImageTask(thumbnailPath, thumbnailUploadProgress, new UploadImageTask.UploadThumbnailHandler() {
             @Override
             public void onSuccess(String url) {
                 lastSelectedThumbnailFile = thumbnailPath;
                 uploadedThumbnailUrl = url;
-                uploading = false;
+                uploadingThumbnail = false;
             }
 
             @Override
@@ -298,7 +362,7 @@ public class GoLiveFragment extends BaseFragment implements
                 showError(getString(R.string.image_upload_failed));
                 lastSelectedThumbnailFile = null;
                 imageThumbnail.setImageDrawable(null);
-                uploading = false;
+                uploadingThumbnail = false;
             }
         });
         task.executeOnExecutor(AsyncTask.THREAD_POOL_EXECUTOR);
@@ -350,6 +414,12 @@ public class GoLiveFragment extends BaseFragment implements
         }
     }
 
+    private void showPrecheckError(String status) {
+        livestreamPrecheckView.setVisibility(View.VISIBLE);
+        imagePrecheckSpaceman.setImageResource(R.drawable.spaceman_sad);
+        textPrecheckStatus.setText(status);
+    }
+
     private void signAndSetupStream() {
         String hexData = Helper.toHexString(selectedChannel.getName());
 
@@ -369,12 +439,24 @@ public class GoLiveFragment extends BaseFragment implements
                     String signingTs = result.getString("signing_ts");
                     if (!Helper.isNullOrEmpty(signature) && !Helper.isNullOrEmpty(signingTs)) {
                         streamKey = createStreamKey(signature, signingTs);
-                        Log.d("MYLOG", streamKey);
                     } else {
-                        showError(getString(R.string.stream_key_not_generated));
+                        new Handler(Looper.getMainLooper()).post(new Runnable() {
+                            @Override
+                            public void run() {
+                                livestreamControlsView.setVisibility(View.GONE);
+                                showPrecheckError(getString(R.string.stream_key_not_generated));
+                            }
+                        });
                     }
                 } catch (ApiCallException | JSONException ex) {
                     showError(ex.getMessage());
+                    new Handler(Looper.getMainLooper()).post(new Runnable() {
+                        @Override
+                        public void run() {
+                            livestreamControlsView.setVisibility(View.GONE);
+                            showPrecheckError(getString(R.string.error_publishing_livestream));
+                        }
+                    });
                 }
             }
         });
@@ -390,16 +472,21 @@ public class GoLiveFragment extends BaseFragment implements
         Claim.StreamMetadata metadata = new Claim.StreamMetadata();
         metadata.setTitle(Helper.getValue(inputTitle.getText()));
 
-        Claim selectedChannel = (Claim) selectChannelSpinner.getSelectedItem();
-        if (selectedChannel != null && !selectedChannel.isPlaceholder() && !selectedChannel.isPlaceholderAnonymous()) {
-            this.selectedChannel = selectedChannel;
-            claim.setSigningChannel(selectedChannel);
-        }
+        claim.setSigningChannel(selectedChannel);
 
         if (!Helper.isNullOrEmpty(uploadedThumbnailUrl)) {
             Claim.Resource thumbnail = new Claim.Resource();
             thumbnail.setUrl(uploadedThumbnailUrl);
             metadata.setThumbnail(thumbnail);
+        } else {
+            // Use the channel thumbnail
+            String channelThumbnailUrl = Optional.ofNullable(selectedChannel.getValue())
+                    .map(Claim.GenericMetadata::getThumbnail)
+                    .map(Claim.Resource::getUrl)
+                    .orElse(null);
+            if (!Helper.isNullOrEmpty(channelThumbnailUrl)) {
+                metadata.setThumbnail(selectedChannel.getValue().getThumbnail());
+            }
         }
 
         claim.setValueType(Claim.TYPE_STREAM);
@@ -412,7 +499,7 @@ public class GoLiveFragment extends BaseFragment implements
         Claim claim = buildLivestreamClaim();
         AccountManager am = AccountManager.get(getContext());
         String authToken = am.peekAuthToken(Helper.getOdyseeAccount(am.getAccounts()), "auth_token_type");
-        PublishClaimTask task = new PublishClaimTask(claim, "", publishLivestreamProgress, authToken, new ClaimResultHandler() {
+        PublishClaimTask task = new PublishClaimTask(claim, "", null, authToken, new ClaimResultHandler() {
             @Override
             public void beforeStart() {
                 buttonToggleStreaming.setEnabled(false);
@@ -421,19 +508,71 @@ public class GoLiveFragment extends BaseFragment implements
             @Override
             public void onSuccess(Claim claimResult) {
                 signAndSetupStream();
+                progress.setVisibility(View.GONE);
                 buttonToggleStreaming.setEnabled(true);
             }
 
             @Override
             public void onError(Exception error) {
-                showError(error.getMessage());
+                if (error != null) {
+                    showError(error.getMessage());
+                }
+                progress.setVisibility(View.GONE);
+                livestreamControlsView.setVisibility(View.GONE);
+                showPrecheckError(getString(R.string.error_publishing_livestream));
             }
         });
         task.executeOnExecutor(AsyncTask.THREAD_POOL_EXECUTOR);
     }
 
+    /**
+     * Checks if it's possible to stream on {@code channel}, and update the
+     * error text with the reason if not.
+     *
+     * @param channel The channel to check
+     * @return True if it's possible to stream on the channel, false otherwise
+     */
+    private boolean checkCanStreamOnChannel(Claim channel) {
+        if (channel == null || channel.isPlaceholder() || channel.isPlaceholderAnonymous()) {
+            return false;
+        }
+
+        if (channel.getConfirmations() < 1) {
+            textChannelError.setText(getString(R.string.channel_error_pending));
+            textChannelError.setVisibility(View.VISIBLE);
+            return false;
+        }
+
+        // Disabled due to lack of server side checking for now
+//        double effectiveAmount = Double.parseDouble(channel.getMeta().getEffectiveAmount());
+//        if (effectiveAmount < MIN_STREAM_STAKE) {
+//            textChannelError.setText(getString(R.string.channel_error_need_minimum_credits,
+//                    MIN_STREAM_STAKE, channel.getName()));
+//            textChannelError.setVisibility(View.VISIBLE);
+//            return false;
+//        }
+
+        textChannelError.setVisibility(View.GONE);
+        return true;
+    }
+
+    private void showLivestreamingOptions() {
+        progress.setVisibility(View.GONE);
+        livestreamPrecheckView.setVisibility(View.GONE);
+        livestreamOptionsView.setVisibility(View.VISIBLE);
+
+        checkCanStreamOnChannel((Claim) selectChannelSpinner.getSelectedItem());
+    }
+
     private void updateChannelList(List<Claim> channels) {
         selectChannelSpinner.setAdapter(new InlineChannelSpinnerAdapter(getContext(), R.layout.spinner_item_channel, channels));
+
+        if (channels.size() > 0) {
+             showLivestreamingOptions();
+        } else {
+            progress.setVisibility(View.GONE);
+            showPrecheckError(getString(R.string.precheck_need_channel));
+        }
     }
 
     private void fetchChannels() {
@@ -454,6 +593,8 @@ public class GoLiveFragment extends BaseFragment implements
                 if (error != null) {
                     showError(error.getMessage());
                 }
+                progress.setVisibility(View.GONE);
+                showPrecheckError(getString(R.string.precheck_error_loading_channels));
             }
         });
         task.executeOnExecutor(AsyncTask.THREAD_POOL_EXECUTOR);
