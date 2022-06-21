a3fe62bde61025310e5f05454bae044bcbf7ccd0
kt programs <ktprograms@gmail.com>
2022-06-21 10:17:55 +0800

    Fix infinite recursion on ChannelCreateDialog showError method
    Call showError on MainActivity and dismiss dialog instead.

--- app/src/main/java/com/odysee/app/ui/channel/ChannelCreateDialogFragment.java
+++ app/src/main/java/com/odysee/app/ui/channel/ChannelCreateDialogFragment.java
@@ -205,7 +205,8 @@ public class ChannelCreateDialogFragment extends BottomSheetDialogFragment {
             activity.runOnUiThread(new Runnable() {
                 @Override
                 public void run() {
-                    showError(message);
+                    activity.showError(message);
+                    dismiss();
                 }
             });
         }
