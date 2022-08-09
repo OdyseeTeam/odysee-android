7454d81c798a4650ba75034b95c90ee3fa94cdb3
Javi Rueda <foss@franjaru.com>
2022-08-09 18:35:48 +0200

    Allow user to hide chat messages on a livestream (#300)

--- app/src/main/java/com/odysee/app/adapter/ChatMessageListAdapter.java
+++ app/src/main/java/com/odysee/app/adapter/ChatMessageListAdapter.java
@@ -7,16 +7,14 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.TextView;
 
+import androidx.annotation.NonNull;
 import androidx.core.content.ContextCompat;
-import androidx.core.text.HtmlCompat;
 import androidx.recyclerview.widget.RecyclerView;
 
-import com.odysee.app.MainActivity;
 import com.odysee.app.R;
 import com.odysee.app.model.Comment;
 import com.odysee.app.utils.Comments;
 import com.odysee.app.utils.Helper;
-import com.odysee.app.utils.LbryUri;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -28,7 +26,8 @@ public class ChatMessageListAdapter extends RecyclerView.Adapter<ChatMessageList
     protected final List<Comment> items;
     @Setter
     private String streamerClaimId;
-    private float scale;
+    private boolean displayMessages;
+    private final float scale;
 
     private static final int CONTAINER_PADDING_REGULAR = 0;
     private static final int CONTAINER_PADDING_HYPERCHAT = 2;
@@ -40,10 +39,16 @@ public class ChatMessageListAdapter extends RecyclerView.Adapter<ChatMessageList
         this.items = new ArrayList<>(items);
         this.context = context;
         this.scale = context.getResources().getDisplayMetrics().density;
+        this.displayMessages = true;
     }
 
     public int getItemCount() {
         return items != null ? items.size() : 0;
+    }
+
+    public void setDisplayMessages(boolean displayMessages) {
+        this.displayMessages = displayMessages;
+        notifyItemRangeChanged(0, getItemCount());
     }
 
     public void addMessage(Comment message) {
@@ -72,33 +77,48 @@ public class ChatMessageListAdapter extends RecyclerView.Adapter<ChatMessageList
         }
     }
 
+    @NonNull
     @Override
-    public ChatMessageListAdapter.ViewHolder onCreateViewHolder(ViewGroup root, int viewType) {
+    public ChatMessageListAdapter.ViewHolder onCreateViewHolder(@NonNull ViewGroup root, int viewType) {
         View v = LayoutInflater.from(context).inflate(R.layout.list_item_chat_message, root, false);
         return new ChatMessageListAdapter.ViewHolder(v);
     }
 
     @Override
-    public void onBindViewHolder(ChatMessageListAdapter.ViewHolder vh, int position) {
-        Comment message = items.get(position);
-        boolean isHyperchat = message.isHyperchat();
+    public void onBindViewHolder(@NonNull ChatMessageListAdapter.ViewHolder vh, int position) {
+        ViewGroup.LayoutParams lp = vh.itemView.getLayoutParams();
 
-        Helper.setViewVisibility(vh.hyperchatHeader, isHyperchat ? View.VISIBLE : View.GONE);
-        vh.highlightContainer.setBackgroundColor(ContextCompat.getColor(context, isHyperchat ? R.color.colorPrimary : android.R.color.transparent));
-        vh.textContainer.setBackgroundColor(ContextCompat.getColor(context, isHyperchat ? R.color.semiTransparentPageBackground : android.R.color.transparent));
+        if (displayMessages) {
+            lp.height = ViewGroup.LayoutParams.WRAP_CONTENT;
+            lp.width = ViewGroup.LayoutParams.MATCH_PARENT;
+            vh.itemView.setLayoutParams(lp);
+            vh.itemView.setVisibility(View.VISIBLE);
 
-        int containerPadding = Helper.getScaledValue(isHyperchat ? CONTAINER_PADDING_HYPERCHAT : CONTAINER_PADDING_REGULAR,  scale);
-        vh.highlightContainer.setPadding(containerPadding, containerPadding, containerPadding, containerPadding);
-        vh.textContainer.setPadding(0, containerPadding, 0, containerPadding);
+            Comment message = items.get(position);
+            boolean isHyperchat = message.isHyperchat();
 
-        Helper.setViewVisibility(vh.creditsIcon, message.isFiat() ? View.GONE : View.VISIBLE);
-        vh.hyperchatValue.setText(message.getHyperchatValue());
+            Helper.setViewVisibility(vh.hyperchatHeader, isHyperchat ? View.VISIBLE : View.GONE);
+            vh.highlightContainer.setBackgroundColor(ContextCompat.getColor(context, isHyperchat && displayMessages? R.color.colorPrimary : android.R.color.transparent));
+            vh.textContainer.setBackgroundColor(ContextCompat.getColor(context, isHyperchat && displayMessages ? R.color.semiTransparentPageBackground : android.R.color.transparent));
 
-        vh.textMessage.setText(Comments.getChatLine(
-                message,
-                streamerClaimId,
-                message.getHandler(),
-                vh.textMessage,
-                context));
+            int containerPadding = Helper.getScaledValue(isHyperchat ? CONTAINER_PADDING_HYPERCHAT : CONTAINER_PADDING_REGULAR,  scale);
+            vh.highlightContainer.setPadding(containerPadding, containerPadding, containerPadding, containerPadding);
+            vh.textContainer.setPadding(0, containerPadding, 0, containerPadding);
+
+            Helper.setViewVisibility(vh.creditsIcon, message.isFiat() ? View.GONE : View.VISIBLE);
+            vh.hyperchatValue.setText(message.getHyperchatValue());
+
+            vh.textMessage.setText(Comments.getChatLine(
+                    message,
+                    streamerClaimId,
+                    message.getHandler(),
+                    vh.textMessage,
+                    context));
+        } else {
+            vh.itemView.setVisibility(View.GONE);
+            lp.height = 0;
+            lp.width = 0;
+            vh.itemView.setLayoutParams(lp);
+        }
     }
 }
