fb53b6652705a24bf32e6c739649a4769d4906c6
kt programs <ktprograms@gmail.com>
2022-11-04 15:28:07 +0800

    chromecast: remove app bar cast button and introductory overlay
    There is no custom web receiver app so casting is only useful when
    playing a video.

--- app/src/full/java/com/odysee/app/utils/CastHelper.java
+++ app/src/full/java/com/odysee/app/utils/CastHelper.java
@@ -8,43 +8,18 @@ import com.google.android.gms.cast.framework.CastButtonFactory;
 import com.google.android.gms.cast.framework.CastContext;
 import com.google.android.gms.cast.framework.CastState;
 import com.google.android.gms.cast.framework.CastStateListener;
-import com.google.android.gms.cast.framework.IntroductoryOverlay;
-import com.odysee.app.R;
 
 public class CastHelper {
     private final Activity context;
     private final CastContext castContext;
     private final CastStateListener castStateListener;
-    private IntroductoryOverlay introductoryOverlay;
 
-    public CastHelper(Activity context, MediaRouteButton introductoryOverlayButton, Listener listener) {
+    public CastHelper(Activity context, Listener listener) {
         this.context = context;
 
         castContext = CastContext.getSharedInstance(context);
 
-        castStateListener = new CastStateListener() {
-            @Override
-            public void onCastStateChanged(int state) {
-                boolean isVisible = state != CastState.NO_DEVICES_AVAILABLE;
-                listener.updateMediaRouteButtonVisibility(isVisible);
-                if (isVisible) {
-                    if (introductoryOverlay != null) {
-                        introductoryOverlay.remove();
-                    }
-                    introductoryOverlay = new IntroductoryOverlay.Builder(context, introductoryOverlayButton)
-                            .setTitleText(R.string.cast_introductory_overlay_text)
-                            .setSingleTime()
-                            .setOnOverlayDismissedListener(new IntroductoryOverlay.OnOverlayDismissedListener() {
-                                @Override
-                                public void onOverlayDismissed() {
-                                    introductoryOverlay = null;
-                                }
-                            })
-                            .build();
-                    introductoryOverlay.show();
-                }
-            }
-        };
+        castStateListener = state -> listener.updateMediaRouteButtonVisibility(state != CastState.NO_DEVICES_AVAILABLE);
     }
 
     public void setUpCastButton(MediaRouteButton mediaRouteButton) {
