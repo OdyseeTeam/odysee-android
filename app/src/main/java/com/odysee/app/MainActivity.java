74cd8f170ef30318a8591f9f5b3a3aea0743496d
Javi Rueda <foss@franjaru.com>
2022-11-28 12:28:10 +0100

    Replace some more newSingleThreadExecutor with OdyseeApp.getexecutor()

--- app/src/main/java/com/odysee/app/MainActivity.java
+++ app/src/main/java/com/odysee/app/MainActivity.java
@@ -164,8 +164,6 @@ import java.util.Random;
 import java.util.concurrent.Callable;
 import java.util.concurrent.CompletableFuture;
 import java.util.concurrent.ExecutionException;
-import java.util.concurrent.ExecutorService;
-import java.util.concurrent.Executors;
 import java.util.concurrent.Future;
 import java.util.concurrent.ScheduledFuture;
 import java.util.concurrent.TimeUnit;
@@ -637,7 +635,7 @@ public class MainActivity extends AppCompatActivity implements SharedPreferences
         }
 
         dbHelper = new DatabaseHelper(this);
-        Executors.newSingleThreadExecutor().execute(new Runnable() {
+        ((OdyseeApp) getApplication()).getExecutor().execute(new Runnable() {
             @Override
             public void run() {
                 SQLiteDatabase db = dbHelper.getWritableDatabase();
@@ -4954,7 +4952,7 @@ public class MainActivity extends AppCompatActivity implements SharedPreferences
     }
 
     public void handleAddUrlToList(String url, OdyseeCollection collection, boolean showMessage) {
-        Executors.newSingleThreadExecutor().execute(new Runnable() {
+        ((OdyseeApp) getApplication()).getExecutor().execute(new Runnable() {
             @Override
             public void run() {
                 try {
@@ -4990,8 +4988,7 @@ public class MainActivity extends AppCompatActivity implements SharedPreferences
     }
 
     public void handleSaveCollection(final OdyseeCollection collection) {
-        ExecutorService executor = ((OdyseeApp) getApplication()).getExecutor();
-        executor.execute(new Runnable() {
+        ((OdyseeApp) getApplication()).getExecutor().execute(new Runnable() {
             @Override
             public void run() {
                 try {
@@ -5025,7 +5022,7 @@ public class MainActivity extends AppCompatActivity implements SharedPreferences
     }
 
     public void handleRemoveUrlFromList(String url, OdyseeCollection collection) {
-        Executors.newSingleThreadExecutor().execute(new Runnable() {
+        ((OdyseeApp) getApplication()).getExecutor().execute(new Runnable() {
             @Override
             public void run() {
                 try {
@@ -5089,7 +5086,7 @@ public class MainActivity extends AppCompatActivity implements SharedPreferences
             return;
         }
 
-        Executors.newSingleThreadExecutor().execute(new Runnable() {
+        ((OdyseeApp) getApplication()).getExecutor().execute(new Runnable() {
             @Override
             public void run() {
                 try {
